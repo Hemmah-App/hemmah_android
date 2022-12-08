cab31734ab3f1a6a708598b17e8b16839c1d106e
Hazem Motaser <hazemmontaser@hotmail.com>
2022-12-08 23:43:48 +0200

    making PostHistoryAdapter template

--- app/src/main/java/com/google/hemmah/PostHistoryAdapter.java
+++ app/src/main/java/com/google/hemmah/PostHistoryAdapter.java
@@ -3,34 +3,34 @@ package com.google.hemmah;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.textfield.TextInputLayout;
-
+import com.google.recyclerview.R;
 
 import java.util.ArrayList;
 
 //we make a custom adapter extending RecyclerView.Adapter
-//And Custom PostHolder that extends RecyclerView.PostHolder
-public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.PostHolder> {
-    private ArrayList<User> dataSample = new ArrayList<>(); //model
+//And Custom PostHistoryAdapterHolder that extends RecyclerView.PostHistoryAdapterHolder
+public class PostHistoryAdapter extends RecyclerView.Adapter<PostHistoryAdapter.PostHistoryAdapterHolder> {
+    private ArrayList<User> dataSample = new ArrayList<>();
 
     @NonNull
     @Override
-    public PostHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public PostHistoryAdapterHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         //constant line in all
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.list_recycler_item, parent, false);
-        return new PostHolder(view);
+                .inflate(R.layout.history_recycler_item, parent, false);
+        return new PostHistoryAdapterHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull PostHolder holder, int position) {
+    public void onBindViewHolder(@NonNull PostHistoryAdapterHolder holder, int position) {
         //use the holder to access to the views with changeable data inside the recyclerview list item
+
 
     }
 
@@ -46,19 +46,17 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.PostHolder> 
 
     }
 
-    public class PostHolder extends RecyclerView.ViewHolder {
+    public class PostHistoryAdapterHolder extends RecyclerView.ViewHolder {
         //put refrence to all the views that its data will change in the runtime
         TextInputLayout title_TextInputLayout;
         TextInputLayout description_TextInputLayout;
         TextView date_TextView;
-        Button okButtonListRecycler;
-        public PostHolder(@NonNull View itemView) {
+        public PostHistoryAdapterHolder(@NonNull View itemView) {
             super(itemView);
             //instantiates the view with its id
             title_TextInputLayout = itemView.findViewById(R.id.title_text_layout);
             description_TextInputLayout = itemView.findViewById(R.id.description_text_layout);
             date_TextView = itemView.findViewById(R.id.choose_date_textView);
-            okButtonListRecycler = itemView.findViewById(R.id.ok_button_list_recycler);
 
         }
     }
