010dd843ebcf2d2df5b256cd0b0ac93d1b53216d
Hazem Motaser <hazemmontaser@hotmail.com>
2022-12-08 16:52:10 +0200

    Login_Activity

--- app/src/main/java/com/google/hemmah/LoginActivity.java
+++ app/src/main/java/com/google/hemmah/LoginActivity.java
@@ -3,12 +3,18 @@ package com.google.hemmah;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    Button logInButton;
+    EditText emailEt;
+    EditText passwordEt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        logInButton =
     }
 }
