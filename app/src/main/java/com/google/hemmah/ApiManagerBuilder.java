7afe833871138c4226f9be6f4b5e0713e2cd6ecd
moaz <https://github.com/moazmahdy/Contacts-app.git>
2022-12-09 02:46:57 +0200

    api manager

---
+++ app/src/main/java/com/google/hemmah/ApiManagerBuilder.java
@@ -0,0 +1,17 @@
+package com.google.hemmah;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiManagerBuilder {
+
+    static Retrofit retrofit;
+
+    static Retrofit getInstance(){
+        if (retrofit == null){
+            retrofit = new Retrofit.Builder().baseUrl("http://192.168.1.7:8080")
+                    .addConverterFactory(GsonConverterFactory.create()).build();
+        }
+        return retrofit;
+    }
+}
