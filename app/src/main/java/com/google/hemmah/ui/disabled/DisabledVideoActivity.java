b9ced5a08a92629fecb3b50007e3da5eafb8a7c2
abdullahsayed <abdullahsayed30@gmail.com>
2023-02-06 16:38:04 +0200

    Correct Content view name

--- app/src/main/java/com/google/hemmah/ui/disabled/DisabledVideoActivity.java
+++ app/src/main/java/com/google/hemmah/ui/disabled/DisabledVideoActivity.java
@@ -58,7 +58,7 @@ public class DisabledVideoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_volunteer_video);
+        setContentView(R.layout.activity_disabled_video);
         initViews();
         setVideoAndAudioSettings();
 
